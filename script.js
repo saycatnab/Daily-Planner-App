426810565ef961b081558a505df3ebce6fd60b2d
saysa <saysaygood83@gmail.com>
2023-09-12 18:23:31 +0100

    added all the hour texts into local storage

--- script.js
+++ script.js
@@ -45,6 +45,30 @@ $(document).ready(function(){
 
     hourUpdate()
 
+    let interval = setInterval(hourUpdate, 15000);
+
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"))
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"))
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"))
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"))
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"))
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"))
+    $("#hour-19 .description").val(localStorage.getItem("hour-19"))
+
+    
+
 })
 
 
